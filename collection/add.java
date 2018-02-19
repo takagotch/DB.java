296789b5397786d702fd87fc1aa3056a2366784c
takagotch <dyaccb@gmail.com>
2018-02-19 15:23:21 +0900

    Collection<String> c2 = new ArrayList<>();

---
+++ collection/add.java
@@ -0,0 +1,13 @@
+Collection<String> c1 = new ArrayList<>();
+for() c1.add();
+
+Collection<> c2 = new ArrayList<>();
+c2.add(); c2.add();
+
+System.out.println();
+System.out.println();
+
+c1.addAll();
+System.out.println();
+
+
